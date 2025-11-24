18d7175fd0c57dfcc0406d0931c2aacc4772ed6e
p7fw7x8sbm-blip <p7fw7x8sbm@privaterelay.appleid.com>
2025-11-24 14:18:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ current_directory = os.getcwd()
 file_path = os.path.join(current_directory, 'winequality-red.csv')
 
 # Read the CSV file into a DataFrame, open file
-df = pd.read_csv(file_path, delimiter=';')
+df = pd.read_csv(file_path, delimiter=',')  # dataframe
 
 # Display the DataFrame in an interactive table
 st.write("Wine Quality Data")
